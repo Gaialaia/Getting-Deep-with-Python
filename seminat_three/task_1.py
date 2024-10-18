e8bde3998c93c02ca9e825faf56f5bba6bbdd118
[Gaia <[dzhayadzoy@gmail.com]>
2024-10-18 14:31:04 +0500

    task_1 seminar_three, edited

--- seminat_three/task_1.py
+++ seminat_three/task_1.py
@@ -1,5 +1,7 @@
 
-
+# Задание 1. Удаление дубликатов из списка
+# Дан список повторяющихся элементов. Вернуть список с дублирующимися
+# элементами. В результирующем списке не должно быть дубликатов
 
 # text = 'Hello, World!'
 #
@@ -7,14 +9,13 @@
 
 
 my_list = [1, 65, 78, 6, 7, 65, 1, 33, 544, 25]
-print(my_list.count(1))
 replica = []
 for i in my_list:
      if my_list.count(i) > 1 and i not in replica:
         replica.append(i)
         my_list.remove(i)
 
-print(f'replica {replica}, list without duplicates {my_list}')
+print(f'replica {replica}, \n list without duplicates {my_list}')
 
 
 
