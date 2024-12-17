449d63ea0b10ea84ecba610363d393be7f1490aa
[Gaia <[dzhayadzoy@gmail.com]>
2024-12-17 18:43:14 +0500

    task_2, datetime

---
+++ homework_fifteen/task_2.py
@@ -0,0 +1,15 @@
+# Задача 2. Работа с текущим временем и датой
+# Напишите скрипт, который получает текущее время и дату, а затем выводит их в
+# формате YYYY-MM-DD HH:MM:SS. Дополнительно, выведите день недели и номер
+# недели в году.
+
+
+from datetime import datetime
+
+now = datetime.now()
+formatted_date = now.strftime('\033[1;32m \ndate: %Y-%m-%d \033[1;33m \ntime: %H:%M:%S '
+                           '\033[1;31m \nweekday: %A \033[1;36m \nweek number: %W \n')
+
+print(formatted_date)
+
+
